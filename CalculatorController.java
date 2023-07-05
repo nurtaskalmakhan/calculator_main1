6fdf758707c041fbadca3e751a054c123e3043b5
nurtaskalmakhan <115890929+nurtaskalmakhan@users.noreply.github.com>
2023-07-05 20:35:32 +0200

    Add files via upload

---
+++ CalculatorController.java
@@ -0,0 +1,35 @@
+package calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return a * b;
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam double a, @RequestParam double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+    }
+}
+
+
